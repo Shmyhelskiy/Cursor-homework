fa0741f60f71edc2f6869c8199d258a8b4b7bf01
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2022-12-28 09:56:08 +0200

    add results

--- Homework-05/Homework-05.js
+++ Homework-05/Homework-05.js
@@ -25,7 +25,6 @@ function getMedian(...numbers) {
       } else return false;
     })
     .sort((a, b) => a - b);
-  let findMidlIndex = 0;
   if (integerArr.length % 2 == 0) {
     return (findMidlIndex =
       (integerArr[integerArr.length / 2] +
@@ -45,41 +44,69 @@ function countPositiveNumbers(...numbers) {
   }, 0);
   return positivNubers;
 }
-
 function getDividedByFive(...numbers) {
   return numbers.filter((item) => item % 5 == 0);
 }
 
 function replaceBadWords(string) {
-  const badWords = ["shit", "fuck"];
+  const badWords = /shit|fuck/gi;
   const arr = string.split(" ");
-
   let newArr = arr.map((item) => {
-    if (item.includes(`shit`)) {
-      return item.replace(`shit`, `****`);
+    if (badWords.test(item)) {
+      return item.replace(badWords, `****`);
     } else return item;
   });
   return newArr.join(" ");
 }
-console.log(replaceBadWords("Holy shiting!, fuck"));
 
-// function findMode(array) {
-//   let object = {};
-//   for (let i = 0; i < array.length; i++) {
-//     if (object[array[i]]) {
-//       object[array[i]] += 1;
-//     } else {
-//       object[array[i]] = 1;
-//     }
-//   }
-//   let biggestValue = 0;
-//   let biggestValuesKey = 0;
-//   let arr = [];
-//   Object.keys(object).forEach((key) => {
-//     let value = object[key];
-//     if (value >= biggestValue) {
-//       biggestValue = value;
-//       biggestValuesKey = key;
-//     }
-//   });
-//   return biggestValuesKey;
+function findMode(array) {
+  let object = {};
+  for (let i = 0; i < array.length; i++) {
+    if (object[array[i]]) {
+      object[array[i]] += 1;
+    } else {
+      object[array[i]] = 1;
+    }
+  }
+  let biggestValue = 0;
+  let biggestValuesKey = 0;
+  Object.keys(object).forEach((key) => {
+    let value = object[key];
+    if (value >= biggestValue) {
+      biggestValue = value;
+      biggestValuesKey = key;
+    }
+  });
+  return biggestValuesKey;
+}
+
+alert(`Приклади в консолі`);
+console.log(
+  `Приклад виклику getRandomArray(length(10), min(1), max(10)): `,
+  getRandomArray(10, 1, 10)
+);
+console.log(`Приклад виклику getAverage(100,200): `, getAverage(100, 200));
+console.log(
+  `Приклад виклику getMedian(1, 2, 3, 4, 5): `,
+  getMedian(1, 2, 3, 4, 5)
+);
+console.log(
+  `Приклад виклику filterEvenNumbers(1, 2, 3, 4, 5): `,
+  filterEvenNumbers(1, 2, 3, 4, 5)
+);
+console.log(
+  `Приклад виклику countPositiveNumbers(1, 2, 3, -1, -2):`,
+  countPositiveNumbers(1, 2, 3, -1, -2)
+);
+console.log(
+  `Приклад виклику getDividedByFive(1, 5, 55, 32):`,
+  getDividedByFive(1, 5, 55, 32)
+);
+console.log(
+  `Приклад виклику replaceBadWords("Holy shit!, fuck"): `,
+  replaceBadWords("Holy shit!, fuck")
+);
+console.log(
+  `Приклад виклику findMode([1, 2, 1, 5]) (Моду шукає тільки одну):`,
+  findMode([1, 2, 1, 5])
+);
